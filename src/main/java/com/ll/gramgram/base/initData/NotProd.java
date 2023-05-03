bae9c79bfee14a96d09c58471c79024673e74b46
ujineeee <yujin11006@naver.com>
2023-05-03 14:59:46 +0900

    refactor : NotProd.java에 있는 내용 application-secret.yml로 옮김

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -6,6 +6,7 @@ import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonServi
 import com.ll.gramgram.boundedContext.member.entity.Member;
 import com.ll.gramgram.boundedContext.member.service.MemberService;
 import com.ll.gramgram.standard.util.Ut;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,18 @@ import java.time.LocalDateTime;
 @Configuration
 @Profile({"dev", "test"})
 public class NotProd {
+    @Value("${custom.security.oauth2.client.registration.google.devUserOauthId}")
+    private String googleDevUserOAuthId;
+
+    @Value("${custom.security.oauth2.client.registration.kakao.devUserOauthId}")
+    private String kakaoDevUserOAuthId;
+
+    @Value("${custom.security.oauth2.client.registration.naver.devUserOauthId}")
+    private String naverDevUserOAuthId;
+
+    @Value("${custom.security.oauth2.client.registration.facebook.devUserOauthId}")
+    private String facebookDevUserOAuthId;
+
     @Bean
     CommandLineRunner initData(
             MemberService memberService,
@@ -34,10 +47,10 @@ public class NotProd {
                 Member memberUser4 = memberService.join("user4", "1234").getData();
                 Member memberUser5 = memberService.join("user5", "1234").getData();
 
-                Member memberUser5ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2733142865").getData();
-                Member memberUser6ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__104542699092917113625").getData();
-                Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__1Hz8OaACOv1KGdT_KkJpOKc1_UeWj4-zs4YzCmqb7H").getData();
-                Member memberUser8ByFacebook = memberService.whenSocialLogin("FACEBOOK", "FACEBOOK__105206845904748").getData();
+                Member memberUser5ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__%s".formatted(kakaoDevUserOAuthId)).getData();
+                Member memberUser6ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__%s".formatted(googleDevUserOAuthId)).getData();
+                Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__%s".formatted(naverDevUserOAuthId)).getData();
+                Member memberUser8ByFacebook = memberService.whenSocialLogin("FACEBOOK", "FACEBOOK__%s".formatted(facebookDevUserOAuthId)).getData();
 
                 instaMemberService.connect(memberUser2, "insta_user2", "M");
                 instaMemberService.connect(memberUser3, "insta_user3", "W");
