e2beec0ba33eb0909531bd501c7d5f130da44174
ujineeee <yujin11006@naver.com>
2023-05-09 16:27:50 +0900

    feat : 내가 받은 호감리스트에서 성별 필터링 기능 구현

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -16,6 +16,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/usr/likeablePerson")
@@ -118,13 +120,30 @@ public class LikeablePersonController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/toList")
-    public String showToList(Model model) {
+    public String showToList(Model model, String gender, @RequestParam(defaultValue = "0") int attractiveTypeCode, @RequestParam(defaultValue = "1") int sortCode) {
         InstaMember instaMember = rq.getMember().getInstaMember();
 
         // 인스타인증을 했는지 체크
         if (instaMember != null) {
+
             // 해당 인스타회원이 좋아하는 사람들 목록
-            List<LikeablePerson> likeablePeople = instaMember.getToLikeablePeople();
+            Stream<LikeablePerson> likeablePeopleStream = instaMember.getToLikeablePeople().stream();
+
+            if(gender != null) {
+                if(gender.equals("W")) {
+                    likeablePeopleStream = likeablePeopleStream.filter(e -> e.getFromInstaMember()
+                                                                            .getGender()
+                                                                            .equals("W"));
+                }
+                else {
+                    likeablePeopleStream = likeablePeopleStream.filter(e -> e.getFromInstaMember()
+                                                                            .getGender()
+                                                                            .equals("M"));
+                }
+            }
+
+            List<LikeablePerson> likeablePeople = likeablePeopleStream.collect(Collectors.toList());
+
             model.addAttribute("likeablePeople", likeablePeople);
         }
 
